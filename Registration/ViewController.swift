418a29a9b1df5ca668b1285105d2cdd5e556ff6d
renjun <renjun.li@thoughtworks.com>
2023-08-14 21:58:56 +0800

    Initial Commit

---
+++ Registration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Registration
+//
+//  Created by Renjun Li on 2023/8/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
